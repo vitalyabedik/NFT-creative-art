5d6d07f89c646a88872eb37165fb49afa83c154c
Vitali Bedik <vitalya.bedik@gmail.com>
2023-08-18 20:13:16 +0300

    init: create project with vite and linters, alias

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import * as path from 'path'
+
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'build',
+  },
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
+})
