1938eb237c4f5688d66e37cfa2e0e0e0d56edf0e
Vitali Bedik <vitalya.bedik@gmail.com>
2023-08-27 11:38:06 +0300

    feat: change logic for PopularImages (GRID)

--- src/layout/sections/popular/popularImages/PopularImages.tsx
+++ src/layout/sections/popular/popularImages/PopularImages.tsx
@@ -45,21 +45,6 @@ export const PopularImages = () => {
           </GridItem>
         )
       })}
-      {/*<GridItem>*/}
-      {/*  <UserInfo userName={'Osvaldo Percy'} />*/}
-      {/*</GridItem>*/}
-      {/*<GridItem>*/}
-      {/*  <UserInfo userName={'Ranson Sqiure'} />*/}
-      {/*</GridItem>*/}
-      {/*<GridItem>*/}
-      {/*  <UserInfo userName={'Sebastian waltan'} />*/}
-      {/*</GridItem>*/}
-      {/*<GridItem>*/}
-      {/*  <UserInfo userName={'Abraham Zack'} />*/}
-      {/*</GridItem>*/}
-      {/*<GridItem>*/}
-      {/*  <UserInfo userName={'Cristio leo'} />*/}
-      {/*</GridItem>*/}
     </GridContainer>
   )
 }
@@ -67,11 +52,10 @@ export const PopularImages = () => {
 const GridContainer = styled.div`
   background-color: red;
   display: grid;
-  grid-template-columns: repeat(129, 10px);
-  grid-template-rows: repeat(2, 311px);
-  row-gap: 30px;
-  justify-content: center;
-  align-content: center;
+  gap: 30px;
+  grid-template-columns: repeat(12, 1fr);
+  grid-template-rows: repeat(2, 310px);
+  margin-top: 64px;
 `
 
 const GridItem = styled.div`
@@ -79,57 +63,22 @@ const GridItem = styled.div`
   position: relative;
 
   &:first-child {
-    grid-area: 1/1/2/31;
-
-    //grid-area: 1/1 / span 1 / span 30;
-
-    //grid-column-start: 1;
-    //grid-column-end: 31;
-    //grid-row-start: 1;
-    //grid-row-end: 2;
+    grid-area: 1/1/1/4;
   }
 
   &:nth-child(2) {
-    grid-area: 1/34/2/86;
-
-    //grid-area: 1/34 / span 1 / span 52;
-
-    //grid-column-start: 34;
-    //grid-column-end: 86;
-    //grid-row-start: 1;
-    //grid-row-end: 2;
+    grid-area: 1/4/1/9;
   }
 
   &:nth-child(3) {
-    grid-area: 2/1/3/42;
-
-    //grid-area: 2/1 / span 1 / span 41;
-
-    //grid-column-start: 1;
-    //grid-column-end: 42;
-    //grid-row-start: 2;
-    //grid-row-end: 3;
+    grid-area: 1/9/3/13;
   }
 
   &:nth-child(4) {
-    grid-area: 2/45/3/86;
-
-    //grid-area: 2/45 / span 1 / span 41;
-
-    //grid-column-start: 45;
-    //grid-column-end: 86;
-    //grid-row-start: 2;
-    //grid-row-end: 3;
+    grid-area: 2/1/3/5;
   }
 
   &:last-child {
-    grid-area: 1/89/3/130;
-
-    //grid-area: 1/89 / span 2 / span 41;
-
-    //grid-column-start: 89;
-    //grid-column-end: 130;
-    //grid-row-start: 1;
-    //grid-row-end: 3;
+    grid-area: 2/5/3/9;
   }
 `
