90ac4a50db59df42077b0cc6f9c8ea6f1ddc56e4
Vitali Bedik <vitalya.bedik@gmail.com>
2023-08-30 21:25:25 +0300

    feat: change adaptive logic for Popular section

--- src/layout/sections/popular/Popular.tsx
+++ src/layout/sections/popular/Popular.tsx
@@ -2,28 +2,33 @@ import * as React from 'react'
 
 import styled from 'styled-components'
 
-import { Container, FlexContainer, SectionHeader, Span } from '@/components'
+import { Button, Container, FlexContainer, SectionHeader, Span } from '@/components'
+import { useWindowWidth } from '@/hooks'
 import { PopularData } from '@layout/sections/popular/popularData/PopularData'
 import { PopularImages } from '@layout/sections/popular/popularImages'
 
 export const Popular: React.FC = () => {
+  const isTablet = useWindowWidth(768)
+
   return (
     <StyledPopular>
       <Container>
-        <FlexContainer direction="column" gap="64px">
-          <SectionHeaderContainer>
-            <SectionHeader
-              title={
-                <div>
-                  Popular <Span>Artists</Span> On This Week
-                </div>
-              }
-              titleWidth="307px"
-              align="center"
-              justify="space-between"
-            />
-          </SectionHeaderContainer>
+        <FlexContainer direction="column" gap={isTablet ? '32px' : '64px'}>
+          <SectionHeader
+            title={
+              <div>
+                Popular <Span>Artists</Span> On This Week
+              </div>
+            }
+            titleWidth="307px"
+            isVisibleButton={isTablet}
+            align="center"
+            justify={isTablet ? 'center' : 'space-between'}
+          />
           <PopularImages popularData={PopularData} />
+          <FlexContainer justify="center">
+            {isTablet && <Button bntType="primary">{'See all'}</Button>}
+          </FlexContainer>
         </FlexContainer>
       </Container>
     </StyledPopular>
@@ -31,9 +36,5 @@ export const Popular: React.FC = () => {
 }
 
 const StyledPopular = styled.section`
-  background-color: #7d3a7d;
-`
-
-const SectionHeaderContainer = styled.section`
-  padding: 0 31.5px;
+  background-color: green;
 `
